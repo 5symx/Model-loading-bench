d30537800bf06ba90a6d9533737ce46a86bfbf28
5symx <mingxuanyang@yeah.net>
2024-09-19 19:38:46 +0000

    add llava_bench_in_the_wild

---
+++ prompt_conv.py
@@ -0,0 +1,130 @@
+
+import dataclasses
+from enum import auto, Enum
+from typing import List, Tuple
+import base64
+from io import BytesIO
+from PIL import Image
+
+class SeparatorStyle(Enum):
+    """Different separator style."""
+    SINGLE = auto()
+    TWO = auto()
+    MPT = auto()
+    PLAIN = auto()
+    LLAMA_2 = auto()
+    
+@dataclasses.dataclass
+class Conversation:
+    """A class that keeps all conversation history."""
+    system: str
+    roles: List[str]
+    messages: List[List[str]]
+    offset: int
+    sep_style: SeparatorStyle = SeparatorStyle.SINGLE
+    sep: str = "###"
+    sep2: str = None
+    version: str = "Unknown"
+
+    skip_next: bool = False
+
+    def get_prompt(self):
+        messages = self.messages
+        if len(messages) > 0 and type(messages[0][1]) is tuple:
+            messages = self.messages.copy()
+            init_role, init_msg = messages[0].copy()
+            init_msg = init_msg[0].replace("<image>", "").strip()
+            if 'mmtag' in self.version:
+                messages[0] = (init_role, init_msg)
+                messages.insert(0, (self.roles[0], "<Image><image></Image>"))
+                messages.insert(1, (self.roles[1], "Received."))
+            else:
+                messages[0] = (init_role, "<image>\n" + init_msg)
+
+        # if self.sep_style == SeparatorStyle.SINGLE:
+        #     ret = self.system + self.sep
+        #     for role, message in messages:
+        #         if message:
+        #             if type(message) is tuple:
+        #                 message, _, _ = message
+        #             ret += role + ": " + message + self.sep
+        #         else:
+        #             ret += role + ":"
+        # elif self.sep_style == SeparatorStyle.TWO:
+        seps = [self.sep, self.sep2]
+        ret = self.system + seps[0]
+        for i, (role, message) in enumerate(messages):
+            if message:
+                if type(message) is tuple:
+                    message, _, _ = message
+                ret += role + ": " + message + seps[i % 2]
+            else:
+                ret += role + ":"
+        # elif self.sep_style == SeparatorStyle.MPT:
+        #     ret = self.system + self.sep
+        #     for role, message in messages:
+        #         if message:
+        #             if type(message) is tuple:
+        #                 message, _, _ = message
+        #             ret += role + message + self.sep
+        #         else:
+        #             ret += role
+        # elif self.sep_style == SeparatorStyle.LLAMA_2:
+        #     wrap_sys = lambda msg: f"<<SYS>>\n{msg}\n<</SYS>>\n\n" if len(msg) > 0 else msg
+        #     wrap_inst = lambda msg: f"[INST] {msg} [/INST]"
+        #     ret = ""
+
+        #     for i, (role, message) in enumerate(messages):
+        #         if i == 0:
+        #             assert message, "first message should not be none"
+        #             assert role == self.roles[0], "first message should come from user"
+        #         if message:
+        #             if type(message) is tuple:
+        #                 message, _, _ = message
+        #             if i == 0: message = wrap_sys(self.system) + message
+        #             if i % 2 == 0:
+        #                 message = wrap_inst(message)
+        #                 ret += self.sep + message
+        #             else:
+        #                 ret += " " + message + " " + self.sep2
+        #         else:
+        #             ret += ""
+        #     ret = ret.lstrip(self.sep)
+        # elif self.sep_style == SeparatorStyle.PLAIN:
+        #     seps = [self.sep, self.sep2]
+        #     ret = self.system
+        #     for i, (role, message) in enumerate(messages):
+        #         if message:
+        #             if type(message) is tuple:
+        #                 message, _, _ = message
+        #             ret += message + seps[i % 2]
+        #         else:
+        #             ret += ""
+        # else:
+        #     raise ValueError(f"Invalid style: {self.sep_style}")
+
+        return ret
+    def copy(self):
+        return Conversation(
+            system=self.system,
+            roles=self.roles,
+            messages=[[x, y] for x, y in self.messages],
+            offset=self.offset,
+            sep_style=self.sep_style,
+            sep=self.sep,
+            sep2=self.sep2,
+            version=self.version)
+    def append_message(self, role, message):
+        self.messages.append([role, message])
+
+conv_vicuna_v1 = Conversation(
+    system="A chat between a curious user and an artificial intelligence assistant. "
+    "The assistant gives helpful, detailed, and polite answers to the user's questions.",
+    roles=("USER", "ASSISTANT"),
+    version="v1",
+    messages=(),
+    offset=0,
+    sep_style=SeparatorStyle.TWO,
+    sep=" ",
+    sep2="</s>",
+)
